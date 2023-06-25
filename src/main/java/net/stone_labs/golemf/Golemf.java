df107a7329c57c181e1676c9d9072da3e5eb9a92
StoneLabs <git@sobadev.com>
2023-06-25 19:38:23 +0200

    Release 1.2.0; Update to MC 1.20.1

--- src/main/java/net/stone_labs/golemf/Golemf.java
+++ src/main/java/net/stone_labs/golemf/Golemf.java
@@ -15,7 +15,7 @@ public class Golemf implements DedicatedServerModInitializer
 
     public static final String MOD_ID = "golemf";
     public static final String MOD_NAME = "GolemF";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
 
     public static boolean disableGolemSpawn = false;
 
